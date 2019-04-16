037b9a490131fb976e5fe6be2ac996e6618d551e
JoaoPBSousa <joao.sousa9397@gmail.com>
2019-04-16 22:54:35 +0100

    updated error messages

--- app/src/main/java/pt/ulisboa/tecnico/cmov/a07/p2photo/NewAlbumActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/a07/p2photo/NewAlbumActivity.java
@@ -2,6 +2,9 @@ package pt.ulisboa.tecnico.cmov.a07.p2photo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Button;
 
 public class NewAlbumActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class NewAlbumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_album);
+
+        ImageButton mFinishView = findViewById(R.id.buttonFinish);
+        mFinishView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAlbum();
+            }
+        });
+
+        Button mAddUserView = findViewById(R.id.buttonAddUser);
+        mAddUserView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+    }
+
+    private void createAlbum(){
+
     }
 }
